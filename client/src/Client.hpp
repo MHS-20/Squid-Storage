904d76c77fa64a09527b09d1e4eaea47c47a36cc
Sentinel <muhamad.salman2001@gmail.com>
2025-03-11 16:38:28 +0100

    (feat) client: created client interface

--- client/src/Client.hpp
+++ client/src/Client.hpp
@@ -1,4 +1,8 @@
-class Client {
-    public: 
-    
-}
+class Client 
+{
+    public:
+        virtual bool connectToServer(int ip);
+        virtual bool createFile(int fileId);
+        virtual bool updateFile(int fileId);
+        virtual bool deleteFile(int fileId);
+}; 
