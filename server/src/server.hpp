0e3f89a3ae03bfea832c6d8fb3da647ca66d5916
Sentinel <muhamad.salman2001@gmail.com>
2025-03-11 17:08:35 +0100

    (feat) server: added server interface methods

--- server/src/server.hpp
+++ server/src/server.hpp
@@ -1,3 +1,4 @@
+#include <vector>
 class Server {
     public: 
         virtual bool listenForClients();
@@ -9,8 +10,8 @@ class Server {
         virtual bool createFile(int fileId);
 
     private: 
-        virtual bool sendFileToDataNode(int fileId);
-        virtual bool receiveFileFromDataNode(int fileId);
-        virtual bool sendFileToClient(int fileId);
-        virtual bool receiveFileFromClient(int fileId);
+        virtual bool replicateFileToDataNodes(int fileId, std::vector<int> ip);
+        virtual bool receiveFileFromDataNode(int fileId, int ip);
+        virtual bool sendFileToClient(int fileId, int ip);
+        virtual bool receiveFileFromClient(int fileId, int ip);
 };
