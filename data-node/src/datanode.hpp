f354d5d7d69ec8551a12e8388b5453dac066705b
Sentinel <muhamad.salman2001@gmail.com>
2025-03-11 17:08:51 +0100

    (feat) datanode: added datanode interface methods

---
+++ data-node/src/datanode.hpp
@@ -0,0 +1,7 @@
+class DataNode{
+
+    public: 
+        virtual bool connectToServer(int ip);
+        virtual bool sendFileToServer(int fileId);
+        virtual bool receiveFileFromServer(int fileId);
+}; 
