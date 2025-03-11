cecebc89a0dc5375da04dce421fe239d2d1fe4f3
Sentinel <muhamad.salman2001@gmail.com>
2025-03-11 16:54:42 +0100

    (refactor) client: change name convention for class files

--- client/src/client.hpp
+++ client/src/client.hpp
@@ -1,8 +1,14 @@
+#include "filelock.hpp"
+
 class Client 
 {
     public:
         virtual bool connectToServer(int ip);
         virtual bool createFile(int fileId);
+        virtual bool requireLock(int fileId);
         virtual bool updateFile(int fileId);
         virtual bool deleteFile(int fileId);
+
+    private: 
+        FileLock file_lock; 
 }; 
