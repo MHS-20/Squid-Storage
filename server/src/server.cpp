fa8d9ac957ee56cb3d76963687f1e7b1febfb7cd
Sentinel <muhamad.salman2001@gmail.com>
2025-03-12 11:50:33 +0100

    (feat) server: splitting socket managment between class methods

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,64 +1,75 @@
-#include "server.hpp"
+#include "iserver.hpp"
 #include <iostream>
 #include <cstring>
 #include <unistd.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-#define PORT 8080
+#define DEFAULT_PORT 8080
 #define BUFFER_SIZE 1024
 
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    socklen_t addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
 
-    std::cout << "Server Starting..." << std::endl;
+class Server : public IServer {
+    private: 
+        int server_fd, new_socket;
+        struct sockaddr_in address;
+        int port;
+        int opt = 1;
+        socklen_t addrlen = sizeof(address);
+        char buffer[BUFFER_SIZE] = {0};
 
-    // 1. Create Socket
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("socket failed");
-        return 1;
-    }
+    public: 
+        Server() : Server(DEFAULT_PORT) {}
 
-    // 2. Socket Options
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+        Server(int port) {
+            this->port = port;
+            server_fd = socket(AF_INET, SOCK_STREAM, 0);
+            if (server_fd == 0) {
+                perror("Socket failed");
+                exit(EXIT_FAILURE);
+            }
 
-    // 3. Address Configuration 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+            setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+        
+            address.sin_family = AF_INET;
+            address.sin_addr.s_addr = INADDR_ANY; 
+            address.sin_port = htons(port);
+        }
 
-    // 4. Bind
-     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        return 1;
-    }
+        ~Server() {
+            close(server_fd);
+        }
 
-    // 5. Listen
-    listen(server_fd, 3);
-    std::cout << "Server listening on " << PORT << "...\n";
+        void start() {
+            std::cout << "Server Starting..." << std::endl;
+            if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+                perror("bind failed");
+                return;
+            }
 
-    // 6. Accept
-    new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
-    if (new_socket < 0) {
-        perror("accept failed");
-        return 1;
-    }
+            listen(server_fd, 3);
+            std::cout << "Server listening on " << this->port << "...\n";
 
-    // 7. Receive
-    read(new_socket, buffer, BUFFER_SIZE);
-    std::cout << "Received: " << buffer << std::endl;
+            while (true) {
+                new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+                if (new_socket < 0) {
+                    perror("Accept failed");
+                    exit(EXIT_FAILURE);
+                }
+        
+                handleClient(new_socket);
+                close(new_socket);
+            }
+        }
 
-    // 8. Reply
-    const char* msg = "Hello from server";
-    send(new_socket, msg, strlen(msg), 0);
-    std::cout << "Message sent\n";
-
-    close(new_socket);
-    close(server_fd);
-    return 0;
-}
+        void handleClient(int client_socket) {
+            char buffer[BUFFER_SIZE] = {0};
+        
+            read(client_socket, buffer, sizeof(buffer));
+            std::cout << "Received: " << buffer << std::endl;
+        
+            const char* message = "Hello from server";
+            send(client_socket, message, strlen(message), 0);
+            std::cout << "Message sent\n";
+        }
+};
