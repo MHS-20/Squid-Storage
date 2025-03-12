568cc5778de6871f71e2094d588793f265141570
Sentinel <muhamad.salman2001@gmail.com>
2025-03-12 12:01:34 +0100

    (feat) datanode: socket managment in class methods

--- data-node/src/datanode.cpp
+++ data-node/src/datanode.cpp
@@ -1,4 +1,4 @@
-#include "datanode.hpp"
+#include "idatanode.hpp"
 #include <iostream>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -8,43 +8,51 @@
 #define SERVER_PORT 8080
 #define BUFFER_SIZE 1024
 
-int main() {
-    int socket_fd = 0;
-    struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
+class DataNode: public IDataNode{
+    private: 
+        int socket_fd = 0;
+        struct sockaddr_in server_addr;
+        char buffer[BUFFER_SIZE] = {0};
 
-    std::cout << "Client starting..." << std::endl;
+    public: 
+        DataNode() : DataNode(SERVER_IP, SERVER_PORT) {}
 
-    // 1. Create
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd < 0) {
-        perror("socket creation failed");
-        return 1;
-    }
-    
-     // 2. Server Address Configuration
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_port = htons(SERVER_PORT);
-     if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
-         perror("invalid server address");
-         return 1;
-     }
- 
-     // 3. Connect
-     if (connect(socket_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-         perror("connection to server failed");
-         return 1;
-     }
- 
-     // 4. Send
-     const char* msg = "Hello from client";
-     send(socket_fd, msg, strlen(msg), 0);
-     std::cout << "Message sent\n";
- 
-     // 5. Receive
-     read(socket_fd, buffer, 1024);
-     std::cout << "Server reply: " << buffer << std::endl;
- 
-     close(socket_fd);
-     return 0;
-}
+        DataNode(const char* server_ip, int port) {
+            socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+            if (socket_fd < 0) {
+                perror("Socket creation failed");
+                exit(EXIT_FAILURE);
+            }
+        
+            server_addr.sin_family = AF_INET;
+            server_addr.sin_port = htons(port);
+
+            if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+                perror("Invalid address");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        ~DataNode() {
+            close(socket_fd);
+        }
+
+        void connectToServer() {
+            if (connect(socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+                perror("connection to server failed");
+                exit(EXIT_FAILURE);
+            }
+            std::cout << "Connected to server...\n";
+        }
+        
+        void sendMessage(const char* message) {
+            send(socket_fd, message, strlen(message), 0);
+            std::cout << "Message sent: " << message << std::endl;
+        }
+        
+        void receiveMessage() {
+            read(socket_fd, buffer, sizeof(buffer));
+            std::cout << "Server Reply: " << buffer << std::endl;
+        }
+
+}; 
