52176c2eeac48cfe9eece2002ac0227ed8395fc4
Sentinel <muhamad.salman2001@gmail.com>
2025-03-12 12:26:11 +0100

    (fix) server: fixed class-interface link

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,75 +1,56 @@
-#include "iserver.hpp"
-#include <iostream>
-#include <cstring>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-
-#define DEFAULT_PORT 8080
-#define BUFFER_SIZE 1024
+#include "server.hpp"
 
 
-class Server : public IServer {
-    private: 
-        int server_fd, new_socket;
-        struct sockaddr_in address;
-        int port;
-        int opt = 1;
-        socklen_t addrlen = sizeof(address);
-        char buffer[BUFFER_SIZE] = {0};
+Server::Server() : Server(DEFAULT_PORT) {}
 
-    public: 
-        Server() : Server(DEFAULT_PORT) {}
+Server::Server(int port) {
+    this->port = port;
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
 
-        Server(int port) {
-            this->port = port;
-            server_fd = socket(AF_INET, SOCK_STREAM, 0);
-            if (server_fd == 0) {
-                perror("Socket failed");
-                exit(EXIT_FAILURE);
-            }
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
-            setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-        
-            address.sin_family = AF_INET;
-            address.sin_addr.s_addr = INADDR_ANY; 
-            address.sin_port = htons(port);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons(port);
+}
+
+Server::~Server() {
+    close(server_fd);
+}
+
+void Server::start() {
+    std::cout << "Server Starting..." << std::endl;
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        return;
+    }
+
+    listen(server_fd, 3);
+    std::cout << "Server listening on " << this->port << "...\n";
+
+    while (true) {
+        new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+        if (new_socket < 0) {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
         }
 
-        ~Server() {
-            close(server_fd);
-        }
+        handleClient(new_socket);
+        close(new_socket);
+    }
+}
 
-        void start() {
-            std::cout << "Server Starting..." << std::endl;
-            if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-                perror("bind failed");
-                return;
-            }
+void Server::handleClient(int client_socket) {
+    char buffer[BUFFER_SIZE] = {0};
 
-            listen(server_fd, 3);
-            std::cout << "Server listening on " << this->port << "...\n";
+    read(client_socket, buffer, sizeof(buffer));
+    std::cout << "Received: " << buffer << std::endl;
 
-            while (true) {
-                new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
-                if (new_socket < 0) {
-                    perror("Accept failed");
-                    exit(EXIT_FAILURE);
-                }
-        
-                handleClient(new_socket);
-                close(new_socket);
-            }
-        }
-
-        void handleClient(int client_socket) {
-            char buffer[BUFFER_SIZE] = {0};
-        
-            read(client_socket, buffer, sizeof(buffer));
-            std::cout << "Received: " << buffer << std::endl;
-        
-            const char* message = "Hello from server";
-            send(client_socket, message, strlen(message), 0);
-            std::cout << "Message sent\n";
-        }
-};
+    const char* message = "Hello from server";
+    send(client_socket, message, strlen(message), 0);
+    std::cout << "Message sent\n";
+}
--- server/src/server.hpp
+++ server/src/server.hpp
@@ -1,8 +1,20 @@
 #include <vector>
 #include <netinet/in.h>
-class IServer {
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define DEFAULT_PORT 8080
+#define BUFFER_SIZE 1024
+
+class Server {
     public: 
         void start();
+        Server(int port);
+        Server();
+        ~Server();
         // virtual bool listenForClients();
         // virtual bool listenForDataNodes();
         // virtual bool lock(int filedId);
@@ -12,6 +24,12 @@ class IServer {
         // virtual bool createFile(int fileId);
 
     private: 
+        int server_fd, new_socket;
+        struct sockaddr_in address;
+        int port;
+        int opt = 1;
+        socklen_t addrlen = sizeof(address);
+        char buffer[BUFFER_SIZE] = {0};
         void handleClient(int client_socket);
         // virtual bool replicateFileToDataNodes(int fileId, std::vector<int> ip);
         // virtual bool receiveFileFromDataNode(int fileId, int ip);
