e57f093e41cca251e7b7124f4c95755c221c94de
Sentinel <muhamad.salman2001@gmail.com>
2025-03-12 12:01:12 +0100

    (feat) datanode: added methods to interface

--- data-node/src/idatanode.hpp
+++ data-node/src/idatanode.hpp
@@ -2,6 +2,8 @@ class IDataNode{
 
     public: 
         virtual bool connectToServer(int ip);
+        virtual void sendMessage(const char* message);
+        virtual void receiveMessage();
         virtual bool sendFileToServer(int fileId);
         virtual bool receiveFileFromServer(int fileId);
 }; 
