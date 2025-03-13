8d1729737960dbdeb47525fb14a99ed2325d6eb6
Sentinel <muhamad.salman2001@gmail.com>
2025-03-13 09:48:38 +0100

    (feat) server: added endpoint mapping

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -17,7 +17,11 @@ Server::Server(int port)
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
-    this->fileTransfer = FileTransfer();
+
+    fileTransfer = FileTransfer();
+    fileMap = std::map<std::string, int>();
+    clientEndpointMap = std::map<std::string, int>();
+    dataNodeEndpointMap = std::map<std::string, int>();
 }
 
 Server::~Server()
@@ -46,17 +50,60 @@ void Server::start()
             exit(EXIT_FAILURE);
         }
 
+        // print client info & update client map
+        
+
+        identifyConnection(new_socket);
         handleClient(new_socket);
-        close(new_socket);
+        // close(new_socket);
     }
 }
 
-void Server::handleClient(int client_socket) {
+void printMap(std::map<std::string, int> &map, std::string name)
+{
+    std::cout << "[SERVER]: " << name << std::endl;
+    for (auto &pair : map)
+    {
+        std::cout << pair.first << " => " << pair.second << std::endl;
+    }
+}
+
+void Server::identifyConnection(int new_socket)
+{
+    char buffer[BUFFER_SIZE] = {0};
+    char connection_ip[INET_ADDRSTRLEN];
+
+    inet_ntop(AF_INET, &address.sin_addr, connection_ip, INET_ADDRSTRLEN);
+    std::string new_endpoint = std::string(connection_ip) + ":" + std::to_string(ntohs(address.sin_port));
+    std::cout << "[SERVER]: Connection from " << new_endpoint << std::endl;
+
+    read(new_socket, buffer, sizeof(buffer));
+    std::cout << "[SERVER]: Connection identified as: " << buffer << std::endl;
+
+    if (strcmp(buffer, "CLIENT") == 0)
+    {
+        clientEndpointMap[new_endpoint] = new_socket;
+    }
+    else if (strcmp(buffer, "DATANODE") == 0)
+    {
+        dataNodeEndpointMap[new_endpoint] = new_socket;
+    }
+    else
+    {
+        std::cout << "[SERVER]: Unknown client type\n";
+    }
+
+    printMap(clientEndpointMap, "CLIENTS MAP");
+    printMap(dataNodeEndpointMap, "DATANODES MAP");
+}
+
+void Server::handleClient(int client_socket)
+{
     receiveFile(client_socket, "./test_txt/received_from_client.txt");
     sendFile(client_socket, "./test_txt/received_from_client.txt");
 }
 
-void Server::handleClientMessages(int client_socket)
+void Server::handleClientMessage(int client_socket)
 {
     char buffer[BUFFER_SIZE] = {0};
 
@@ -69,10 +116,13 @@ void Server::handleClientMessages(int client_socket)
 }
 
 /* ---- FILE TRANSFER API ----- */
-void Server::sendFile(int client_socket, const char *filepath){
+void Server::sendFile(int client_socket, const char *filepath)
+{
     this->fileTransfer.sendFile(client_socket, "[SERVER]", filepath);
 }
 
-void Server::receiveFile(int client_socket, const char *outputpath){
+void Server::receiveFile(int client_socket, const char *outputpath)
+{
     this->fileTransfer.receiveFile(client_socket, "[SERVER]", outputpath);
 }
+
--- server/src/server.hpp
+++ server/src/server.hpp
@@ -6,40 +6,48 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <fstream>
+#include <map>
 #include "filetransfer.hpp"
 
 #define DEFAULT_PORT 8080
 #define BUFFER_SIZE 1024
 
-class Server {
-    public: 
-        void start();
-        Server(int port);
-        Server();
-        ~Server();
-        void receiveFile(int client_socket, const char* outputpath);
-        void sendFile(int client_socket, const char* filepath);
-        // virtual bool listenForClients();
-        // virtual bool listenForDataNodes();
-        // virtual bool lock(int filedId);
-        // virtual bool unlock(int fileId);
-        // virtual bool updateFile(int fileId);
-        // virtual bool deleteFile(int fileId);
-        // virtual bool createFile(int fileId);
+class Server
+{
+public:
+    void start();
+    Server(int port);
+    Server();
+    ~Server();
+    void receiveFile(int client_socket, const char *outputpath);
+    void sendFile(int client_socket, const char *filepath);
+    // virtual bool listenForClients();
+    // virtual bool listenForDataNodes();
+    // virtual bool lock(int filedId);
+    // virtual bool unlock(int fileId);
+    // virtual bool updateFile(int fileId);
+    // virtual bool deleteFile(int fileId);
+    // virtual bool createFile(int fileId);
 
-    private: 
-        int server_fd, new_socket;
-        struct sockaddr_in address;
-        int port;
-        int opt = 1;
-        socklen_t addrlen = sizeof(address);
-        char buffer[BUFFER_SIZE] = {0};
-        FileTransfer fileTransfer;
+private:
+    int port;
+    int opt = 1;
+    char buffer[BUFFER_SIZE] = {0};
 
-        void handleClient(int client_socket);
-        void handleClientMessages(int client_socket); 
-        // virtual bool replicateFileToDataNodes(int fileId, std::vector<int> ip);
-        // virtual bool receiveFileFromDataNode(int fileId, int ip);
-        // virtual bool sendFileToClient(int fileId, int ip);
-        // virtual bool receiveFileFromClient(int fileId, int ip);
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+
+    FileTransfer fileTransfer;
+    std::map<std::string, int> fileMap;             // filename to file id
+    std::map<std::string, int> clientEndpointMap;   // client ip/name to socket_fd
+    std::map<std::string, int> dataNodeEndpointMap; // datanode ip/name to socket_fd
+
+    void identifyConnection(int client_socket);
+    void handleClient(int client_socket);
+    void handleClientMessage(int client_socket);
+    // virtual bool replicateFileToDataNodes(int fileId, std::vector<int> ip);
+    // virtual bool receiveFileFromDataNode(int fileId, int ip);
+    // virtual bool sendFileToClient(int fileId, int ip);
+    // virtual bool receiveFileFromClient(int fileId, int ip);
 };
