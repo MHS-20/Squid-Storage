f4ea056121b383696293f12677beb0e57554ab9e
Gabribrele <gabrielebasigli@gmail.com>
2025-03-18 17:07:42 +0100

    (feat)squid protocol: protocol formatter interface

---
+++ common/squidprotocolformatter.hpp
@@ -0,0 +1,35 @@
+#include <string>
+
+enum ProtocolKeyWord
+{
+    CREATE_FILE,
+    TRANSFER_FILE,
+    READ_FILE,
+    UPDATE_FILE,
+    DELETE_FILE,
+    ACQUIRE_LOCK,
+    RELEASE_LOCK,
+    HEARTBEAT,
+    SYNC_STATUS,
+    IDENTIFY,
+    RESPONSE,
+};
+
+class SquidProtocolFormatter
+{
+public:
+    std::string createFileFormat(std::string filePath);
+    std::string transferFileFormat(std::string fileContent);
+    std::string readFileFormat(std::string filePath);
+    std::string updateFileFormat(std::string filePath);
+    std::string deleteFileFormat(std::string filePath);
+    std::string acquireLockFormat(std::string filePath);
+    std::string releaseLockFormat(std::string filePath);
+    std::string heartbeatFormat();
+    std::string syncStatusFormat();
+    std::string identifyFormat();
+    std::string responseFormat();
+
+private:
+    std::string createMessage(ProtocolKeyWord keyword, std::vector<std::string> args);
+};
