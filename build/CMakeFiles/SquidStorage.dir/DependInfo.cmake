037ef89d8f27954e331eb2dd827e6e0a25ebb127
Basigli <gabrielebasigli@gmail.com>
2025-04-10 21:07:16 +0200

    (fix) gui: fixed build for linux env

--- build/CMakeFiles/SquidStorage.dir/DependInfo.cmake
+++ build/CMakeFiles/SquidStorage.dir/DependInfo.cmake
@@ -8,17 +8,17 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/gabe/Documents/GitHub/Squid-Storage/common/src/filesystem/filelock.cpp" "CMakeFiles/SquidStorage.dir/common/src/filesystem/filelock.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/common/src/filesystem/filelock.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/common/src/filesystem/filemanager.cpp" "CMakeFiles/SquidStorage.dir/common/src/filesystem/filemanager.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/common/src/filesystem/filemanager.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/imgui/backends/imgui_impl_opengl2.cpp" "CMakeFiles/SquidStorage.dir/imgui/backends/imgui_impl_opengl2.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/backends/imgui_impl_opengl2.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/imgui/backends/imgui_impl_sdl2.cpp" "CMakeFiles/SquidStorage.dir/imgui/backends/imgui_impl_sdl2.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/backends/imgui_impl_sdl2.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/imgui/imgui.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/imgui/imgui_demo.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui_demo.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/imgui/imgui_draw.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui_draw.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/imgui/imgui_tables.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui_tables.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/imgui/imgui_widgets.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui_widgets.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/imgui/src/Application.cpp" "CMakeFiles/SquidStorage.dir/imgui/src/Application.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/src/Application.cpp.o.d"
-  "/Users/gabe/Documents/GitHub/Squid-Storage/imgui/src/main.cpp" "CMakeFiles/SquidStorage.dir/imgui/src/main.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/src/main.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/common/src/filesystem/filelock.cpp" "CMakeFiles/SquidStorage.dir/common/src/filesystem/filelock.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/common/src/filesystem/filelock.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/common/src/filesystem/filemanager.cpp" "CMakeFiles/SquidStorage.dir/common/src/filesystem/filemanager.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/common/src/filesystem/filemanager.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/imgui/backends/imgui_impl_opengl2.cpp" "CMakeFiles/SquidStorage.dir/imgui/backends/imgui_impl_opengl2.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/backends/imgui_impl_opengl2.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/imgui/backends/imgui_impl_sdl2.cpp" "CMakeFiles/SquidStorage.dir/imgui/backends/imgui_impl_sdl2.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/backends/imgui_impl_sdl2.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/imgui/imgui.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/imgui/imgui_demo.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui_demo.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/imgui/imgui_draw.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui_draw.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/imgui/imgui_tables.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui_tables.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/imgui/imgui_widgets.cpp" "CMakeFiles/SquidStorage.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/imgui_widgets.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/imgui/src/Application.cpp" "CMakeFiles/SquidStorage.dir/imgui/src/Application.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/src/Application.cpp.o.d"
+  "/home/gabry/repos/Squid-Storage/imgui/src/main.cpp" "CMakeFiles/SquidStorage.dir/imgui/src/main.cpp.o" "gcc" "CMakeFiles/SquidStorage.dir/imgui/src/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
